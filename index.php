7845031cd139465fea4e67ceddef55fb93e60d78
PollyPocket <Malleapaulline@gmail>
2016-06-26 20:16:18 -0400

    second commit: adding gmail sign up

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Post Date Email Service For Friends &amp; Family</title>
+	<link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/custom.css">
+
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:700,400' rel='stylesheet' type='text/css'>
+
+	<meta name="google-signin-client_id" content="sendLove.apps.googleusercontent.com">
+
+
+</head>
+<body>
+<!--Video Background-->
+	  	<video autoplay loop muted poster="screenshot.jpg" id="background">
+        	<source src="videos/Family_Video_4.mp4" type="video/mp4">
+    	</video>
+<!--Navigation-->
+<div class="container">
+	<div class="row">
+	<div class="col-md-4">
+	</div>
+	<div class="col-md-4">
+	</div>
+	  <div class="col-md-4">
+	  	<NAV>
+			<ul class="nav">
+				<li><a href="About.html">About</a></li>
+				<li><a href="Signup.html">Sign Up</a></li>
+				<li><a href="Login.html">Log In</a></li></li>
+			</ul>
+		</NAV>
+
+	  
+	  </div>
+	</div>
+
+<!--Homepage Selling Pitch with Sign up & Learn more button-->
+	<div class="row">
+	  <div class="col-md-12">
+	  	<h1>Send Love</h1>
+	  	<p>Leave a leagacy of love beyond your lifetime.</p>
+
+	  	<!-- <div style="width:50%; margin:0 auto"> -->
+	  		<!-- <button id="signupBtn" type="button" class="btn btn-primary btn-lg active">
+  			Sign Up
+			</button>  -->
+		<div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+	<!-- 	<span><a href="About.html"> Learn more </a></span> -->
+
+	  </div>
+
+	</div>
+</div>
+
+<div class="container2">
+	<div class="row">
+	<div class="col-md-12">
+			<div> <img src="images/border.jpg" alt="Envlope Style Border" style="width:100%;"> </div>
+	</div>
+	</div>
+</div>
+<!-- About Section -->
+
+<div class="container3">
+	<div class="row">
+	<div class="col-md-3">
+	</div>
+
+	<div class="col-md-6" style="margin-top: 50px;">
+		<h1>You can... </h1>
+
+		<p>Send Love beyond your lifetime to all the poeple who have filled your life with beautiful memories. Create short video messages that will mean the world to them and help them cope after you have gone.
+		</p>
+
+	</div>
+	<div class="col-md-3">
+	</div>	
+	</div>
+
+	<div class="row">
+	<div class="col-md-3">
+	</div>
+
+	<div class="col-md-8">
+	<h1>How it works? </h1>
+
+	 <p>Create video & set a delivery date. It's that simple!
+	</p>
+
+	</div>
+	<div class="col-md-1">
+	</div>	
+	</div>
+	</div>
+</div>
+
+	<div class="row">
+	<div class="col-md-12">
+		<p id= "footer"> copyright protected </p>
+	</div>
+	</div>
+</div>
+
+<!-- scripts -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+
+
+
+
+</body>
+</html>
---
+++ python/simple_apiAccess.py
@@ -0,0 +1,4 @@
+from apiclient.discovery import build
+
+API_KEY = AIzaSyChdcvyOXxLmSD1r3tTGf9hWDzravt5Vao
+SERVICE = build(API,VERSION, developerKEY = API_KEY)
